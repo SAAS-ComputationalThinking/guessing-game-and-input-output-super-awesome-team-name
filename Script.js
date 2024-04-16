b9f75063b22ce7d5a7ffc043ff9e10c67a3cc52a
Keokigopal <156035015+Keokigopal@users.noreply.github.com>
2024-04-16 12:54:32 -0700

    Made input work

--- Script.js
+++ Script.js
@@ -1,4 +1,11 @@
-function getRandomInt(2) {
-    return math.floor(math.random() * max);
-}
-console.log(getRandomInt(2))
+//function getRandomInt(max) {
+    //return math.floor(math.random() * max);
+//}
+//console.log(getRandomInt(5));
+const max=math.floor(math.random()*5);
+const input = document.getElementById("myInput");
+const inputValue = input.value;
+console.log(inputValue);
+if (max > inputValue) {
+    console.log("Nick is an adult.");
+    }
