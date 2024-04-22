b866fac9bcf975dd7aed0b34823a512f7056f704
Keokigopal <156035015+Keokigopal@users.noreply.github.com>
2024-04-22 12:41:11 -0700

    commit

--- Script.js
+++ Script.js
@@ -2,10 +2,31 @@
     //return math.floor(math.random() * max);
 //}
 //console.log(getRandomInt(5));
-const max=Math.floor(Math.random()*5);
+const max=Math.floor(Math.random()*1000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000);
+var btn = document.getElementById("");
+// btn.addEventListener("Click,enterbutton")
+function enterbutton(){
 const input = document.getElementById("myInput");
 const inputValue = input.value;
-console.log(inputValue);
-if (max > inputValue) {
-    console.log("Nick is an adult.");
+
+
+
+ if (max > inputValue) {
+    //window.print("Your too low");
+    var html="<br/>Your too low";
+     document.getElementById('feedback').innerHTML=html;
+    
+
+    }else if(inputValue==max){
+       // window.print("You got it!");
+       var html="<br/>You got it!";
+       document.getElementById('feedback').innerHTML=html;
+    
+
+    }else{
+       // window.print("Your too High")
+       var html="<br/>Your too high!";
+        document.getElementById('feedback').innerHTML=html;
     }
+        
+}
